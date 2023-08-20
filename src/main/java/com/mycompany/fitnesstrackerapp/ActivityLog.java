b4471a0f44b254a544628890127fd56d931fa93e
VihoHuang <VihoHuang@192.168.2.34>
2023-08-20 14:25:39 -0400

    test

--- src/main/java/com/mycompany/fitnesstrackerapp/ActivityLog.java
+++ src/main/java/com/mycompany/fitnesstrackerapp/ActivityLog.java
@@ -4,10 +4,19 @@
  */
 package com.mycompany.fitnesstrackerapp;
 
+import java.util.Scanner;
+
 /**
  *
  * @author VihoHuang
  */
 public class ActivityLog {
+    
+    Scanner input = new Scanner(System.in);
+    String penis = input.next();
+    
+    public void penis() {
+        System.out.println(penis);
+    }
     
 }
