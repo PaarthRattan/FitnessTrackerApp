2fb5e7ec7f13b1f364c1a8b72c0181d4eecb2e35
Paarth Rattan <139022835+PaarthRattan@users.noreply.github.com>
2023-09-01 17:17:38 -0400

    Update Exercise.java

--- src/main/java/com/mycompany/fitnesstrackerapp/Exercise.java
+++ src/main/java/com/mycompany/fitnesstrackerapp/Exercise.java
@@ -5,16 +5,24 @@
 package com.mycompany.fitnesstrackerapp;
 
 /**
- *
- * @author sande
+ * Represents an exercise in the fitness tracker app.
+ * This class stores information about the exercise, including name, calories burned,
+ * focus area, and whether it requires a gym.
  */
 public class Exercise {
-    //instance variables
+    // Instance variables
     String name; 
     int caloriesBurned;
     String focus;
     String gymRequired;
        
+    /**
+     * Constructor for creating an Exercise object.
+     * @param name The name of the exercise.
+     * @param caloriesBurned The calories burned per session of the exercise.
+     * @param focus The focus area of the exercise (e.g., cardio, strength, flexibility).
+     * @param gymRequired Whether the exercise requires a gym (yes or no).
+     */
     public Exercise(String name, int caloriesBurned, String focus, String gymRequired) {
         this.name = name;
         this.caloriesBurned = caloriesBurned;
